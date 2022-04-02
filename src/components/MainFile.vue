e5a9b6d8ca54bfb2eae963f9830fe31b69fee9f6
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-02 14:44:48 +0200

    Mvp come as u are section

--- src/components/MainFile.vue
+++ src/components/MainFile.vue
@@ -1,13 +1,47 @@
 <template>
-  <main></main>
+  <main>
+    <div class="come_as_u_are">
+      <div class="container py-5 h-100 d-flex">
+        <div class="col-7 h-100">
+          <div class="container_mex bg-light p-5 h-100">
+            <h1>Come as you are</h1>
+            <p>
+              MasterStudy used is an excellent workshop whether you come as a
+              counselor, advisor, administrator, or faculty member. I am going
+              home empowered. I am looking forward to attending the On Course
+              MasterStudy and the MasterStudy National Conference next year and
+              facilitating the use of this excellent retention/student success
+              course at my college.
+            </p>
+            <p>Tim Sab</p>
+            <p>General Developer, Stylemix Themes</p>
+          </div>
+        </div>
+        <div class="col-3 d-flex align-items-center ">
+          <div class="triangle">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-    name: 'MainFile',
-}
+  name: "MainFile",
+};
 </script>
 
 <style lang="scss" scoped>
-
+.come_as_u_are {
+  height: 70vh;
+  background-image: url("../assets/images/brooke-cagle-224821-unsplash-1-1920x700.jpg");
+  .triangle{
+    border-left: 50px solid whitesmoke;
+    border-right: 50px solid transparent;
+    border-top: 50px solid transparent;
+    border-bottom: 50px solid transparent;
+  }
+}
 </style>
