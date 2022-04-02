30a719bd4f44921210fe87f36866612d6ca7bff0
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-02 11:14:49 +0200

    essential header-btm

--- src/components/HeaderFile.vue
+++ src/components/HeaderFile.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <div class="header_top">
+        <div class="header_top border-bottom">
             <div class="my_container d-flex align-items-center">
                 <select name="" id="" class="me-auto">
                     <option value="english">English</option>
@@ -26,6 +26,32 @@
                 </ul>
             </div>
         </div>
+        <div class="header-btm">
+            <div class="my_container d-flex align-items-center py-3">
+
+                <img src="../assets/images/MasterStudy_logo.svg" alt="master-study_logo" class="me-4">
+
+                <div class="search_section mx-4 d-flex">
+                    <select name="" id="">
+                        <option value="">CATEGORY</option>
+                    </select>
+                    <input type="search" name="" id="" placeholder="Search courses">
+                    <div><i class="fa-solid fa-magnifying-glass bg-primary text-light p-2"></i></div>
+                </div>
+
+                <div class="work_with_us mx-4 d-flex justify-content-between">
+                    <span class="p-2"><i class="fa-solid fa-bullhorn mx-1"></i>Become an Instructor</span>
+                    <span class="p-2"><i class="fa-solid fa-briefcase mx-1"></i>For Enterprise</span>
+                </div>
+
+                <div class="log-in_section ms-4 d-flex justify-content-between">
+                    <span class="p-2"><i class="fa-regular fa-user mx-1"></i>Log-in</span>
+                    <span class="common_btn p-2">sign-up</span>
+                    <a href="#" class="p-2"><i class="fa-regular fa-bookmark"></i></a>
+                </div>
+
+            </div>
+        </div>
     </header>
 </template>
 
@@ -39,6 +65,21 @@ export default {
 .my_container{
     width: 1800px;
     margin: 0 auto;
+}
+
+.search_section{
+    width: 50%;
+    input{
+        width: 400px;
+    }
+}
+
+.work_with_us{
+    width: 25%;
+}
+
+.log-in_section{
+    width: 25%;
 }
 </style>
         
