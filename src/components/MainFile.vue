345b84615db285427974610de1eeff2c1f97051c
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-03 15:27:35 +0200

    Mvp newsletter

--- src/components/MainFile.vue
+++ src/components/MainFile.vue
@@ -177,6 +177,34 @@
     </div>
 
     <!-- Section newsletter -->
+    <div class="newsletter bg-success">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-6">
+            <div class="py-5">
+              <h2>Subscribe our newsletter</h2>
+              <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words</p>
+            </div>
+          </div>
+
+          <div class="col-6">
+            <div class="py-5">
+              <h2 class="text-uppercase">
+                your e-mail address
+              </h2>
+              <div>
+                <input type="email" name="" id="" placeholder="Enter your E-mail">
+                <div class="bg-primary text-uppercase d-inline">
+                  subscribe
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
 
     <!-- Section popular courses -->
 
