c18359d3ee3effab02c117d3e4aacceba10ce949
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-04 01:22:09 +0200

    Refactoring moe-info

--- src/components/CoursesCategoryFile.vue
+++ src/components/CoursesCategoryFile.vue
@@ -6,22 +6,22 @@
             <div class="row">
                 <div class="col-8">
                     <img src="../assets/images/cat_1-1540x750.jpg" alt="" class="img-fluid">
-                    <div class="py-4 d-flex justify-content-between">
-                    <span>Software Deevelopment</span>
-                    <span>5 Courses</span>
+                    <div class="txt_wrapper d-flex justify-content-between">
+                        <span>Software Deevelopment</span>
+                        <span>5 Courses</span>
                     </div>
                 </div>
             
                 <div class="col-4">
                     <img src="../assets/images/cat_2-740x310.jpg" alt="" class="img-fluid">
-                    <div class="py-4 d-flex justify-content-between">
-                    <span>Art</span>
-                    <span>8 Courses</span>
+                    <div class="txt_wrapper d-flex justify-content-between">
+                        <span>Art</span>
+                        <span>8 Courses</span>
                     </div>
                     <img src="../assets/images/cat_3-740x310.jpg" alt="" class="img-fluid">
-                    <div class="py-4 d-flex justify-content-between">
-                    <span>Material Design</span>
-                    <span>5 Courses</span>
+                    <div class="txt_wrapper d-flex justify-content-between">
+                        <span>Material Design</span>
+                        <span>5 Courses</span>
                     </div>
                 </div>
             </div>
@@ -29,26 +29,26 @@
             <div class="row">
             
                 <div class="col-4">
-                    <img src="../assets/images/cat_4-740x310.jpg" alt="" class="img-fluid">
-                    <div class="py-4 d-flex justify-content-between">
-                    <span>Music</span>
-                    <span>5 Courses</span>
+                    <img src="../assets/images/cat_5-740x310.jpg" alt="" class="img-fluid">
+                    <div class=" txt_wrapper d-flex justify-content-between">
+                        <span>Music</span>
+                        <span>5 Courses</span>
                     </div>
                 </div>
             
                 <div class="col-4">
-                    <img src="../assets/images/cat_5-740x310.jpg" alt="" class="img-fluid">
-                    <div class="py-4 d-flex justify-content-between">
-                    <span>Exercise</span>
-                    <span>7 Courses</span>
+                    <img src="../assets/images/cat_4-740x310.jpg" alt="" class="img-fluid">
+                    <div class="txt_wrapper d-flex justify-content-between">
+                        <span>Exercise</span>
+                        <span>7 Courses</span>
                     </div>
                 </div>
             
                 <div class="col-4">
                     <img src="../assets/images/cat_6-740x310.jpg" alt="" class="img-fluid">
-                    <div class="py-4 d-flex justify-content-between">
-                    <span>Photography</span>
-                    <span>4 Courses</span>
+                    <div class="txt_wrapper d-flex justify-content-between">
+                        <span>Photography</span>
+                        <span>4 Courses</span>
                     </div>
                 </div>
             </div>
@@ -64,5 +64,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .wrapper_s{
+        margin-bottom: 7rem;
+    }
+    img{
+        margin-bottom: 1rem;
+    }
+    .txt_wrapper{
+        padding-bottom: 22px;
+    }
 </style>
