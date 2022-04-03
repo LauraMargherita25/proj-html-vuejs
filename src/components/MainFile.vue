dde3d9700a222541a07fcdd2e0e44401b0766105
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-03 13:42:22 +0200

    Mvp courses category

--- src/components/MainFile.vue
+++ src/components/MainFile.vue
@@ -54,7 +54,62 @@
       </div>
     </div>
 
-    
+    <!-- Section courses category -->
+    <div class="courses_category">
+      <div class="container py-5">
+        
+        <div class="row">
+          <div class="col-8">
+            <img src="../assets/images/cat_1-1540x750.jpg" alt="" class="img-fluid">
+            <div class="py-4 d-flex justify-content-between">
+              <span>Software Deevelopment</span>
+              <span>5 Courses</span>
+            </div>
+          </div>
+          
+          <div class="col-4">
+            <img src="../assets/images/cat_2-740x310.jpg" alt="" class="img-fluid">
+            <div class="py-4 d-flex justify-content-between">
+              <span>Art</span>
+              <span>8 Courses</span>
+            </div>
+            <img src="../assets/images/cat_3-740x310.jpg" alt="" class="img-fluid">
+            <div class="py-4 d-flex justify-content-between">
+              <span>Material Design</span>
+              <span>5 Courses</span>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          
+          <div class="col-4">
+            <img src="../assets/images/cat_4-740x310.jpg" alt="" class="img-fluid">
+            <div class="py-4 d-flex justify-content-between">
+              <span>Music</span>
+              <span>5 Courses</span>
+            </div>
+          </div>
+          
+          <div class="col-4">
+            <img src="../assets/images/cat_5-740x310.jpg" alt="" class="img-fluid">
+            <div class="py-4 d-flex justify-content-between">
+              <span>Exercise</span>
+              <span>7 Courses</span>
+            </div>
+          </div>
+          
+          <div class="col-4">
+            <img src="../assets/images/cat_6-740x310.jpg" alt="" class="img-fluid">
+            <div class="py-4 d-flex justify-content-between">
+              <span>Photography</span>
+              <span>4 Courses</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
 
     <!-- Section work with us -->
     <div class="work_with_us">
