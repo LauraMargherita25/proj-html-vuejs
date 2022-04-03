ace43efb2e12f2a0130074d65f64801cabaa3a96
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-03 15:15:03 +0200

    Mvp  we are proud section

--- src/components/MainFile.vue
+++ src/components/MainFile.vue
@@ -128,7 +128,57 @@
       </div>
     </div>
 
-    
+    <!-- Section recent courses -->
+
+    <!-- Section we are proud -->
+    <div class="we_are_proud bg-info">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-12">
+            <div class="text-center py-5">
+              <h1>We are Proud</h1>
+              <p>Certificate courses are instructed by highly educated and qualified instructors who hold doctoral and master's level degrees.</p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row">
+          <div class="col-3">
+            <div class="text-center border-end">
+              <p>2000</p>
+              <p>STUDENTS</p>
+            </div>
+          </div>
+
+          <div class="col-3">
+            <div class="text-center border-end">
+              <p>950</p>
+              <p>COURSE</p>
+            </div>
+          </div>
+
+          <div class="col-3">
+            <div class="text-center border-end">
+              <p>1600</p>
+              <p>HOURS VIDEO</p>
+            </div>
+          </div>
+
+          <div class="col-3">
+            <div class="text-center">
+              <p>150</p>
+              <p>COUNTRIES RACHED</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Section newsletter -->
+
+    <!-- Section popular courses -->
 
     <!-- Section work with us -->
     <div class="work_with_us">
@@ -209,6 +259,13 @@ export default {
 .more_info{
   height: 60vh;
   background-image: url("../assets/images/Untitled-1-1-1-1-1.png");
+}
+
+.we_are_proud{
+  height: 70vh;
+  background-image: url("../assets/images/we_proud.png");
+  background-position: bottom;
+  background-repeat: no-repeat;
 }
 
 .come_as_u_are {
