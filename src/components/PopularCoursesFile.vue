68d5f66ead2e96c5952464ee26f3b8da98ea6113
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-04 15:36:07 +0200

    Refactoringpopular courses

--- src/components/PopularCoursesFile.vue
+++ src/components/PopularCoursesFile.vue
@@ -1,12 +1,25 @@
 <template>
+
     <!-- Section popular courses -->
-    <div class="popular_courses">
-        <div class="wrapper_l py-5">
+    <div class="bg">
+        <div class="wrapper_l">
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="txt_wrapper">
+                        <h2>Popular courses</h2>
+                        <p>Discover our most popular courses for self learning</p>
+                    </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-12">
-                    <div class="text-center">
-                    <h1>Popular courses</h1>
-                    <p>Discover our most popular courses for self learning</p>
+                    <div class="btn_direction">
+                        <div class="arrow left">
+                            <i class="fa-solid fa-angle-left"></i>
+                        </div>
+                        <div class="arrow right">
+                            <i class="fa-solid fa-angle-right"></i>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -21,5 +34,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/styles/partials/variables.scss";
 
+    .bg{
+        background-color: $light_blue;
+        .col-8{
+            .txt_wrapper{
+                margin: 6rem 0 4rem;
+                text-align: center;
+                h2{
+                    margin-bottom: 2rem;
+                }
+                p{
+                    font-size: 18px;
+                    font-weight: 200;
+                }
+            }
+        }
+        .btn_direction{
+            display: flex;
+            justify-content: center;
+            margin-bottom: 5rem;
+            .arrow{
+                width: 50px;
+                height: 50px;
+                border: 1px solid $light_gray;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                color: $light_gray;
+                background-color: white;
+                &:hover{
+                    background-color: #457992;
+                    color: white;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
 </style>
