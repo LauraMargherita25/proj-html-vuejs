fb5676128f3c6e03bb5215faa6012bf52f7d62e1
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-04 16:52:56 +0200

    Refactoring feed

--- src/components/FeedFile.vue
+++ src/components/FeedFile.vue
@@ -1,23 +1,23 @@
 <template>
     <!-- Setion feed -->
-    <div class="come_as_u_are">
-        <div class="wrapper_s py-5 h-100 d-flex">
-            <div class="col-7 h-100">
-                <div class="container_mex bg-light p-5 h-100">
-                    <h1>Come as you are</h1>
-                    <p>
-                    MasterStudy used is an excellent workshop whether you come as a
-                    counselor, advisor, administrator, or faculty member. I am going
-                    home empowered. I am looking forward to attending the On Course
-                    MasterStudy and the MasterStudy National Conference next year and
-                    facilitating the use of this excellent retention/student success
-                    course at my college.
-                    </p>
-                    <p>Tim Sab</p>
-                    <p>General Developer, Stylemix Themes</p>
-                </div>
+    <div class="bg">
+        <div class="wrapper_s">
+
+            <div class="container_mex">
+                <h3>Come as you are</h3>
+                <p>
+                MasterStudy used is an excellent workshop whether you come as a
+                counselor, advisor, administrator, or faculty member. I am going
+                home empowered. I am looking forward to attending the On Course
+                MasterStudy and the MasterStudy National Conference next year and
+                facilitating the use of this excellent retention/student success
+                course at my college.
+                </p>
+                <strong>Tim Sab</strong>
+                <small class="d-block">General Developer, Stylemix Themes</small>
             </div>
-            <div class="col-3 d-flex align-items-center ">
+
+            <div class="d-flex align-items-center">
                 <div class="triangle"></div>
             </div>
         </div>
@@ -31,14 +31,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .come_as_u_are {
-        height: 70vh;
+    .bg{
+        height: 680px;
         background-image: url("../assets/images/brooke-cagle-224821-unsplash-1-1920x700.jpg");
-        .triangle{
-            border-left: 50px solid whitesmoke;
-            border-right: 50px solid transparent;
-            border-top: 50px solid transparent;
-            border-bottom: 50px solid transparent;
+        background-position: bottom;
+        .wrapper_s{
+            padding: 5rem 0;
+            height: 100%;
+            display: flex;
+            .container_mex{
+                width: 735px;
+                height: 100%;
+                background-color: white;
+                padding: 5rem;
+                p{
+                    margin: 2rem 0;
+                    font-size: 18px;
+                    font-weight: 200;
+                }
+            }
+            .triangle{
+                border-left: 25px solid white;
+                border-right: 25px solid transparent;
+                border-top: 18px solid transparent;
+                border-bottom: 18px solid transparent;
+            }
         }
     }
 </style>
