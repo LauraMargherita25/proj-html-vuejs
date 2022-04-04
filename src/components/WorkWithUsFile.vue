d12c669a45138627ac45527f3b138914f6572f4c
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-04 16:10:35 +0200

    Refactoring work with us

--- src/components/WorkWithUsFile.vue
+++ src/components/WorkWithUsFile.vue
@@ -1,38 +1,38 @@
 <template>
+    
     <!-- Section work with us -->
-    <div class="work_with_us">
-        <div class="wrapper_l py-5">
-            <div class="row">
+    <div class="wrapper_l">
+        <div class="row">
 
-                <div class="col-6">
-                    <div class="border p-5">
-                        <img src="../assets/images/image_box_1-221x231.png" alt="">
-                        <div class="d-inline-block ps-3">
-                            <h1>Become an Instructor</h1>
-                            <p>Teach what you love. Masterstudy gives you the tools to create a course.</p>
-                            <div class="common_btn">
-                            start taching
-                            </div>
+            <div class="col-6">
+                <div class="card_wwf">
+                    <img src="../assets/images/image_box_1-221x231.png" alt="">
+                    <div class="txt_wrapper">
+                        <h3>Become an Instructor</h3>
+                        <p>Teach what you love. Masterstudy gives you the tools to create a course.</p>
+                        <div class="common_btn">
+                        start taching
                         </div>
                     </div>
                 </div>
-
-                <div class="col-6">
-                    <div class="border p-5">
-                        <img src="../assets/images/image_box_2-234x231.png" alt="">
-                        <div class="d-inline-block ps-3">
-                            <h1>Setup For Business</h1>
-                            <p>Get unlimited access to 2,500 of Udemy's top courses for your team.</p>
-                            <div class="common_btn">
-                            doing business
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             </div>
+
+            <div class="col-6">
+                <div class="card_wwf">
+                    <img src="../assets/images/image_box_2-234x231.png" alt="">
+                    <div class="txt_wrapper">
+                        <h3>Setup For Business</h3>
+                        <p>Get unlimited access to 2,500 of Udemy's top courses for your team.</p>
+                        <div class="common_btn">
+                        doing business
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
+    
 </template>
 
 <script>
@@ -42,5 +42,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/styles/partials/variables.scss";
 
+    .wrapper_l{
+        padding: 6rem 0;
+        .card_wwf{
+            padding: 5rem 12rem 4rem 2rem;
+            border: 1px solid $light_gray;
+            display: flex;
+            img{
+                margin-right: 2.5rem;
+            }
+            .txt_wrapper{
+                h3{
+                    margin-top: 1.5rem
+                }
+                p{
+                    font-size: 18px;
+                    font-weight: 200;
+                    margin: 1.5rem 0;
+                }
+            }
+        }
+    }
 </style>
