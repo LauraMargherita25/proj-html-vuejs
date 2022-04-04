9358b795b21b1cfd8a9e4499235a243ea765edf3
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-04 10:13:33 +0200

    Refactoring recent courses

--- src/components/RecentCoursesFile.vue
+++ src/components/RecentCoursesFile.vue
@@ -1,27 +1,39 @@
 <template>
+    
     <!-- Section recent courses -->
-    <div class="recent_courses">
-        <div class="wrapper_l">
-            <div class="row">
-                <div class="col-12">
-                    <div class="text-center">
-                        <h2>Recnt courses</h2>
-                        <nav>
-                            <ul class="list-inline">
-                            <li class="list-inline-item px-2">All Categories</li>
-                            <li class="list-inline-item px-2">Art</li>
-                            <li class="list-inline-item px-2">Exercise</li>
-                            <li class="list-inline-item px-2">Material Design</li>
-                            <li class="list-inline-item px-2">Music</li>
-                            <li class="list-inline-item px-2">Photograohy</li>
-                            <li class="list-inline-item px-2">Software Development</li>
-                            </ul>
-                        </nav>
-                    </div>
+    <div class="wrapper_l">
+        <div class="row">
+            <div class="col-12">
+                
+                <h2>Recnt courses</h2>
+                <nav>
+                    <ul>
+                        <li>All Categories</li>
+                        <li>Art</li>
+                        <li>Exercise</li>
+                        <li>Material Design</li>
+                        <li>Music</li>
+                        <li>Photograohy</li>
+                        <li>Software Development</li>
+                    </ul>
+                </nav>
+                
+            </div>
+
+            <div class="col-12">
+                <div class="card_container">
+
+                </div>
+            </div>
+
+            <div class="col-12 text-center">
+                <div class="common_btn">
+                    show all
                 </div>
             </div>
         </div>
     </div>
+    
 </template>
 
 <script>
@@ -31,5 +43,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import "../assets/styles/partials/variables.scss";
+    .wrapper_l{
+        margin-bottom: 7rem;
+        h2{
+            margin-bottom: 3rem;
+            text-align: center;
+        }
+        ul{
+            margin-bottom: 3rem;
+            text-align: center;
+            li{
+                padding: 1rem;
+                display: inline;
+                color: $blue;
+                font-size: 15px;
+                &:hover{
+                    background-color: $light_blue;
+                    color: $gray;
+                    border-radius: 50px;
+                }
+            }
+        }
+        .card_container{
+            margin-bottom: 3rem;
+        }
+    }
 </style>
