569edad8411cfa7ef16d654ceeea8a356ab0356d
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-05 10:09:18 +0200

    Refactoring header btm

---
+++ src/components/HeaderBtmFile.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="header-btm">
+            <div class="wrapper_l d-flex align-items-center">
+
+                <img src="../assets/images/MasterStudy_logo.svg" alt="master-study_logo" >
+
+                <div class="search_section d-flex">
+                    <div class="select">
+                        <i class="fa-solid fa-bars"></i>
+                        <span>CATEGORY</span>
+                    </div>
+                    <div class="search">
+                        <input type="search" name="" id="" placeholder="Search courses">
+                        <div class="search_btn"><i class="fa-solid fa-magnifying-glass"></i></div>
+                    </div>
+                </div>
+
+                <div class="work_with_us">
+                    <span><i class="fa-solid fa-bullhorn"></i>Become an Instructor</span>
+                    <span><i class="fa-solid fa-briefcase"></i>For Enterprise</span>
+                </div>
+
+                <div class="account_section">
+                    <span class="log-in"><i class="fa-regular fa-user"></i>Log-in</span>
+                    <span class="common_btn">sign-up</span>
+                    <span class="fav"><i class="fa-regular fa-bookmark"></i></span>
+                </div>
+
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderBtmFile"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/partials/variables.scss";
+
+    .header-btm{
+        margin-top: 1rem;
+    }
+   img{
+       margin-right: 5rem;
+   }
+   
+   .search_section{
+       margin-right: 5rem;
+        .select{
+            display: flex;
+            align-items: center;
+
+            margin-right: 1rem;
+
+            color: $gray;
+            font-size: 12px;
+            font-weight: 600;
+            i{
+                color: $gray;
+                margin-right: .5rem;
+            }
+        }
+        .search{
+            width: 500px;
+            display: flex;
+            input{
+                width: 100%;
+                border: none;
+                text-indent: 10px;
+                &::placeholder{
+                    background-color: #f0f2f5;
+                }
+            }
+            .search_btn{
+                padding: .8rem 1.5rem;
+                background-color: $blue;
+                color: white;
+            }
+        }
+    }
+   
+    .work_with_us{
+        margin-right: 5rem;
+        font-size: 14px;
+        span{
+            margin-right: 1.2rem;
+            i{
+                color: $blue;
+                margin-right: .5rem;
+            }
+        }
+    }
+   
+    .account_section{
+        color: $blue;
+        .log-in{
+            font-size: 14px;
+            i{
+                margin-right: .5rem;
+            }
+        }
+        .common_btn{
+            width: 150px;
+            margin: 0 1.5rem;
+        }
+        .fav{
+            font-size: 25px;
+        }
+    }
+
+</style>
--- src/components/HeaderFile.vue
+++ src/components/HeaderFile.vue
@@ -1,124 +1,28 @@
 <template>
     <header>
-        <div class="header_top border-bottom">
-            <div class="wrapper_l">
-                <nav class="d-flex align-items-center">
-                    <select name="" id="" class="me-auto">
-                        <option value="english">English</option>
-                    </select>
-                    <ul class="d-flex">
-                        <li class="p-2"><a href="#">Courses</a></li>
-                        <li class="p-2">
-                            <span class="position-absolute top-0 end-0  badge bg-danger">
-                                NEW
-                            </span>
-                            <a href="#">Zoom</a>
-                        </li>
-                        <li class="p-2"><a href="#">Pages</a></li>
-                        <li class="p-2"><a href="#">Bundles</a></li>
-                        <li class="p-2"><a href="#">CourseFormats</a></li>
-                        <li class="p-2">
-                            <a href="#">Add Course</a>
-                            <span class="position-absolute top-0 end-0  badge bg-danger">
-                                NEW
-                            </span>
-                        </li>
-                        <li class="p-2"><a href="#">Demos</a></li>
-                    </ul>
-                    <ul class="d-flex">
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-behance-square"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-dribbble"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-flickr"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-git"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
-                        <li class="p-2"><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-        <div class="header-btm">
-            <div class="wrapper_l d-flex align-items-center py-3">
-
-                <img src="../assets/images/MasterStudy_logo.svg" alt="master-study_logo" class="me-4">
-
-                <div class="search_section mx-4 d-flex">
-                    <select name="" id="">
-                        <option value="">CATEGORY</option>
-                    </select>
-                    <input type="search" name="" id="" placeholder="Search courses">
-                    <div><i class="fa-solid fa-magnifying-glass bg-primary text-light p-2"></i></div>
-                </div>
-
-                <div class="work_with_us mx-4 d-flex justify-content-between">
-                    <span class="p-2"><i class="fa-solid fa-bullhorn mx-1"></i>Become an Instructor</span>
-                    <span class="p-2"><i class="fa-solid fa-briefcase mx-1"></i>For Enterprise</span>
-                </div>
-
-                <div class="log-in_section ms-4 d-flex justify-content-between">
-                    <span class="p-2"><i class="fa-regular fa-user mx-1"></i>Log-in</span>
-                    <span class="common_btn p-2">sign-up</span>
-                    <a href="#" class="p-2"><i class="fa-regular fa-bookmark"></i></a>
-                </div>
-
-            </div>
-        </div>
+        <header-top-file />
+        <header-btm-file />
+        
     </header>
 </template>
 
 <script>
+import HeaderTopFile from './HeaderTopFile.vue';
+import HeaderBtmFile from './HeaderBtmFile.vue';
+
 export default {
     name: 'HeaderFile',
+
+    components: {
+        HeaderTopFile,
+        HeaderBtmFile,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/styles/partials/variables.scss";
-header{
+    header{
     margin-bottom: 5rem
-}
-
-.header_top{
-    padding: .5rem 0;
-    color: $gray;
-    font-size: 13px;
-    select{
-        font-size: 13px;
-        color: $gray;
-        border: none;
-        background-color: white;
-    }
-    li{
-        position: relative;
-        a{
-            line-height: 30px;
-        }
-        .badge{
-            padding: 3px;
-            font-size: 9px;
-            font-weight: 400;
-            border-radius: 0;
-        }
-    }
-    i{
-        font-size: 15px;
-    }
-}
-
-.search_section{
-    width: 50%;
-    input{
-        width: 400px;
-    }
-}
-
-.work_with_us{
-    width: 25%;
-}
-
-.log-in_section{
-    width: 25%;
 }
 </style>
         
